41d5747f361ebf8ec0a5fdeb91528815fcb2da1a
s.nozaki <nozaki.sho@dinamica.co.jp>
2022-11-15 23:26:02 +0900

    pass/[183]customers_who_never_order

---
+++ code/183_Customers_Who_Never_Order.sql
@@ -0,0 +1,30 @@
+-- Active: 1668306365956@@127.0.0.1@3306@leetcode
+
+Create table If Not Exists Customers (id int, name varchar(255));
+
+Create table If Not Exists Orders (id int, customerId int);
+
+Truncate table Customers;
+
+insert into Customers (id, name) values ('1', 'Joe');
+
+insert into Customers (id, name) values ('2', 'Henry');
+
+insert into Customers (id, name) values ('3', 'Sam');
+
+insert into Customers (id, name) values ('4', 'Max');
+
+Truncate table Orders;
+
+insert into Orders (id, customerId) values ('1', '3');
+
+insert into Orders (id, customerId) values ('2', '1');
+
+select * from `Customers`;
+
+select * from `Orders`;
+
+select name as Customers
+from Customers as a
+    left join Orders as b on a.id = b.customerId
+where b.customerId is null;
