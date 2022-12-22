a224aaac761d9690092cbb0ad85e8ad000038050
nossy20 <s.nozaki.japan@gmail.com>
2022-12-22 19:30:30 +0900

    pass/[184]department_highest_salary

---
+++ code/184_Department_Highest_Salary.sql
@@ -0,0 +1,54 @@
+-- Active: 1668306365956@@127.0.0.1@3306@leetcode
+
+Create table
+    If Not Exists Employee (
+        id int,
+        name varchar(255),
+        salary int,
+        departmentId int
+    );
+
+Create table If Not Exists Department (id int, name varchar(255));
+
+Truncate table Employee;
+
+insert into
+    Employee (id, name, salary, departmentId)
+values ('1', 'Joe', '70000', '1');
+
+insert into
+    Employee (id, name, salary, departmentId)
+values ('2', 'Jim', '90000', '1');
+
+insert into
+    Employee (id, name, salary, departmentId)
+values ('3', 'Henry', '80000', '2');
+
+insert into
+    Employee (id, name, salary, departmentId)
+values ('4', 'Sam', '60000', '2');
+
+insert into
+    Employee (id, name, salary, departmentId)
+values ('5', 'Max', '90000', '1');
+
+Truncate table Department;
+
+insert into Department (id, name) values ('1', 'IT');
+
+insert into Department (id, name) values ('2', 'Sales');
+
+select
+    b.name as "Department",
+    a.name as "Employee",
+    a.salary as "Salary"
+from `Employee` as a
+    left outer join `Department` as b on a.departmentId = b.id
+where (a.departmentId, a.salary) in (
+        select
+            departmentId,
+            max(salary)
+        from `Employee`
+        group by
+            departmentId
+    );
